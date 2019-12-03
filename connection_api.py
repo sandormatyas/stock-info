5c0467e012584853cc617b8ba2670b65e4e21c25
Gergo Kovacs <kovacsgergo128@gmail.com>
2019-12-03 21:08:23 +0100

    Upgrade general fetch function and add api calling functions of main page

--- connection_api.py
+++ connection_api.py
@@ -1,7 +1,26 @@
 import requests
+import os
 # fetches data from an api
 
 
-def fetch(url):
-    response = requests.get(url).json()
+def fetch_yahoo_fin(url_end, params=None):
+    url = "https://apidojo-yahoo-finance-v1.p.rapidapi.com/" + url_end
+    headers = {
+        'x-rapidapi-host': "apidojo-yahoo-finance-v1.p.rapidapi.com",
+        'x-rapidapi-key': os.environ.get('YAHOO_API_KEY')
+    }
+    response = requests.get(url, headers=headers, params=params).json()
+
     return response
+
+
+def get_quotes(params):
+    url_end = "market/get-quotes"
+
+    return fetch_yahoo_fin(url_end, params=params)
+
+
+def get_stock_detail(params):
+    url_end = "stock/get-detail"
+
+    return fetch_yahoo_fin(url_end, params=params)
