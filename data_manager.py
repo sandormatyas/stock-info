f9ff31966c6ad6658e2dafe4b9af723fd087a403
Gergo Kovacs <kovacsgergo128@gmail.com>
2019-12-03 21:09:58 +0100

    Add SQL query to get tickers by user

--- data_manager.py
+++ data_manager.py
@@ -1,1 +1,15 @@
+import connection_db
 # sql queries come here
+
+
+def get_tickers_by_user(user_id):
+    query = '''
+            SELECT ticker
+            FROM users_tickers
+            WHERE user_id = %(user_id)s;
+            '''
+    params = {
+        'user_id': user_id
+    }
+
+    return connection_db.execute_query(query, params=params)
