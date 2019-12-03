18637b1db06f8a0c9d214af1e347dd23e619ad75
sandormatyas <sandormatyas7@gmail.com>
2019-12-03 12:48:34 +0100

    start register story

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 import util
 
 app = Flask(__name__)
@@ -7,6 +7,11 @@ app = Flask(__name__)
 @app.route('/')
 def index():
     return render_template('index.html')
+
+
+@app.route('/register', methods=['POST'])
+def register():
+    user_data = request.form.to_dict()
 
 
 if __name__ == '__main__':
