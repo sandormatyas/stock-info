91d04d5da25885dff5a932f42d688d72133ea327
sandormatyas <sandormatyas7@gmail.com>
2019-12-03 14:39:06 +0100

    add queries related to registration and login

--- data_manager.py
+++ data_manager.py
@@ -1,1 +1,20 @@
-# sql queries come here
+import connection_db
+
+
+@connection_db.connection_handler
+def register_user(cursor, user_data):
+    cursor.execute("""
+        INSERT INTO users (username, password)
+        VALUES (%(username)s, %(password)s)
+    """, {'username': user_data['username'], 'password': user_data['password']})
+
+
+@connection_db.connection_handler
+def get_hashed_password(cursor, username):
+    cursor.execute("""
+        SELECT password
+        FROM users
+        WHERE username = %(username)s
+    """, {'username': username})
+    password = cursor.fetchone()
+    return password['password']  # so it returns a string instead of Dict
