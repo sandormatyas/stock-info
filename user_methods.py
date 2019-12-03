3c8e241de8cb8d2f8ff57133559b60cf3bd6df11
sandormatyas <sandormatyas7@gmail.com>
2019-12-03 14:40:16 +0100

    add user register and login methods

--- user_methods.py
+++ user_methods.py
@@ -1,4 +1,16 @@
+import data_manager
 import bcrypt
+
+
+def handle_register(user_data):
+    user_data['password'] = hash_password(user_data['password'])
+    data_manager.register_user(user_data)
+
+
+def handle_login(user_data):
+    plain_text_password = user_data['password']
+    hashed_password = data_manager.get_hashed_password(user_data['username'])
+    return verify_password(plain_text_password, hashed_password)
 
 
 def hash_password(plain_text_password):
