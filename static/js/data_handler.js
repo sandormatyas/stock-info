cef990c965edcfb88be0942088525ee7a415985f
sandormatyas <sandormatyas7@gmail.com>
2019-12-03 10:28:38 +0100

    add data_handler layer to fetch data from server

---
+++ static/js/data_handler.js
@@ -0,0 +1,30 @@
+export let dataHandler = {
+    _api_get: function (url, callback) {
+        // loads data from API, parses it and calls the callback with it
+
+        fetch(url, {
+            method: 'GET',
+            credentials: 'same-origin'
+        })
+        .then(response => response.json())  // parse the response as JSON
+        .then(json_response => callback(json_response));  // Call the `callback` with the returned object
+    },
+    _api_post: function (url, data, callback) {
+        // sends the data to the API, and calls callback function
+
+        fetch(url, {
+            method: 'POST',
+            credentials: 'same-origin',
+            headers: new Headers({
+                'content-type': 'application/json'
+            }),
+            body: JSON.stringify(data)
+        })
+        .then(response => response.json())
+        .then(json_response => callback(json_response));
+
+    },
+    getStock: function (stockTicker, callback) {
+        // example
+    }
+};
