ab92d45dd0108914f11a1e9312177f49cf14feb6
sandormatyas <sandormatyas7@gmail.com>
2019-12-03 14:38:42 +0100

    create login, logout and register routes

--- app.py
+++ app.py
@@ -1,7 +1,9 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, redirect, session
 import util
+import user_methods
 
 app = Flask(__name__)
+app.secret_key = b'_5#y2L"F4Q8z\n\xec]/'
 
 
 @app.route('/')
@@ -12,7 +14,22 @@ def index():
 @app.route('/register', methods=['POST'])
 def register():
     user_data = request.form.to_dict()
+    user_methods.handle_register(user_data)
+    return redirect('/')
 
+
+@app.route('/login', methods=['POST'])
+def login():
+    user_data = request.form.to_dict()
+    if user_methods.handle_login(user_data):
+        session['username'] = user_data['username']
+    return redirect('/')
+
+
+@app.route('/logout')
+def logout():
+    session.pop('username')
+    return redirect('/')
 
 if __name__ == '__main__':
     app.run(debug=True)
