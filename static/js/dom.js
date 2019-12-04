fbefd10e6d39b95f78e1c58e71142cc69470f6be
RolandOver9000 <csosz.roland2@gmail.com>
2019-12-04 17:43:36 +0100

    modified table, tried to add arrows

--- static/js/dom.js
+++ static/js/dom.js
@@ -9,15 +9,36 @@ function showMainTable() {
     let tbody = document.querySelector("tbody");
     dataHandler._api_get('/stocks', function (data) {
         for (let row of data) {
+
+            //decide which arrow should we use
+            if (row['trends']['longTermTrend'] === 'UP'){
+                row['trends']['longTermTrend'] = `<i class="fas fa-angle-double-up"></i>`
+            }
+            else {
+                row['trends']['longTermTrend'] = `<i class="fas fa-angle-double-down"></i>`
+            }
+
+            if (row['trends']['midTermTrend'] === 'UP'){
+                row['trends']['midTermTrend'] = `<i class="fas fa-angle-double-up"></i>`
+            }
+            else {
+                row['trends']['midTermTrend'] = `<i class="fas fa-angle-double-down"></i>`
+            }
+
+            if (row['trends']['shortTermTrend'] === 'UP'){
+                row['trends']['shortTermTrend'] = `<i class="fas fa-angle-double-up"></i>`
+            }
+            else {
+                row['trends']['shortTermTrend'] = `<i class="fas fa-angle-double-down"></i>`
+            }
+
+
             let rowTemplate = document.getElementById('data-row-template').innerHTML;
             let compiledTemplate = Handlebars.compile(rowTemplate);
             console.log(row);
             let renderedTemplate = compiledTemplate(row);
             tbody.insertAdjacentHTML("beforeend", renderedTemplate);
-        }
-    })
-
-
+    }})
 }
 
 showMainTable();
