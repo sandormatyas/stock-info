aadef031718a00c9d30622a232c047b4bbbb9d8a
sandormatyas <sandormatyas7@gmail.com>
2019-12-04 10:31:38 +0100

    add minor refactoring

--- user_methods.py
+++ user_methods.py
@@ -10,9 +10,9 @@ def handle_register(user_data):
 
 
 def handle_login(user_data):
-    plain_text_password = user_data['password']
     hashed_password = data_manager.get_hashed_password(user_data['username'])
-    is_verified = verify_password(plain_text_password, hashed_password)
+    is_verified = verify_password(user_data['password'], hashed_password)
+
     if is_verified:
         timestamp = datetime.datetime.now().replace(microsecond=0)
         user_id = data_manager.update_time_get_user_id(user_data['username'], timestamp)
