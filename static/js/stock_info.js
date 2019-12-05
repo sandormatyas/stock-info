f9ede18aee52ed01f4dba5b996afe39e8b35c401
sandormatyas <sandormatyas7@gmail.com>
2019-12-05 13:57:25 +0100

    implement stock info page

--- static/js/stock_info.js
+++ static/js/stock_info.js
@@ -1,26 +1,30 @@
-import {dataHandler} from "./data_handler";
+import {dataHandler} from "./data_handler.js";
 
 export let stockLoader = {
-    loadStockInfo: function (ticker) {
+    loadStockInfo: function (event) {
         //clear the page
+        const ticker = event.currentTarget.dataset.ticker;
         dataHandler.getStock(ticker, stockData => {
-            const stockPage = this.createStockTemplate(stockData)
-            dataHandler.getNews(ticker, news => {
-                this.insertNews(stockPage, news)
-            })
+            const stockPage = this.createStockTemplate(stockData);
+            const page = document.querySelector('#content');
+            page.innerHTML = "";
+            page.appendChild(stockPage);
         })
     },
     createStockTemplate: function (stockData) {
         const template = document.querySelector('#stock-info-template');
         const templateCopy = document.importNode(template.content, true);
+        const stockInfo = stockData[0];
 
-        templateCopy.querySelector('#stock-name').textContent = stockData.summaryProfile.price.longName;
-        templateCopy.querySelector('#stock-description').textContent = stockData.summaryProfile.longBusinessSummary;
-        templateCopy.querySelector('#stock-country').textContent = stockData.summaryProfile.country;
-        templateCopy.querySelector('#stock-sector').textContent = stockData.summaryProfile.sector;
-        templateCopy.querySelector('#stock-industry').textContent = stockData.summaryProfile.industry;
-        templateCopy.querySelector('#stock-website').textContent = stockData.summaryProfile.website;
-        templateCopy.querySelector('#stock-employees').textContent = stockData.summaryProfile.fullTimeEmployees;
+        templateCopy.querySelector('#stock-name').textContent = stockInfo.longName;
+        templateCopy.querySelector('#stock-description').textContent = stockInfo.description;
+        templateCopy.querySelector('#stock-country').textContent = stockInfo.country;
+        templateCopy.querySelector('#stock-sector').textContent = stockInfo.sector;
+        templateCopy.querySelector('#stock-industry').textContent = stockInfo.industry;
+        templateCopy.querySelector('#stock-website').textContent = stockInfo.website;
+        templateCopy.querySelector('#stock-employees').textContent = stockInfo.employees;
+
+        this.insertNews(templateCopy, stockData[1]);
 
         return templateCopy
     },
@@ -32,12 +36,16 @@ export let stockLoader = {
             const newsTemplateCopy = document.importNode(newsTemplate.content, true);
 
             newsTemplateCopy.querySelector('#news-sentiment').textContent = news_story.sentiment;
-            newsTemplateCopy.querySelector('#news-picture').textContent = news_story.picture;
+            newsTemplateCopy.querySelector('#news-picture').src = news_story.img_url;
             newsTemplateCopy.querySelector('#news-title').textContent = news_story.title;
-            newsTemplateCopy.querySelector('#news-publisher').textContent = news_story.publisher;
-            newsTemplateCopy.querySelector('#news-date').textContent = news_story.date;
+            newsTemplateCopy.querySelector('#news-title').href = news_story.news_url;
+            newsTemplateCopy.querySelector('#news-publisher').textContent = news_story.source;
+            newsTemplateCopy.querySelector('#news-date').textContent = news_story.published;
 
-            newsBlock.insertAdjacentElement('beforeend', newsTemplateCopy);
+            newsBlock.appendChild(newsTemplateCopy);
         }
+    },
+    insertChart: function (stockPage, chartData) {
+        
     }
 };
